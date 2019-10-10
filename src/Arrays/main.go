87f4c1cc98720afa6eaca98248137408b4f89158
Harish Konda <harish.konda92@gmail.com>
2019-10-11 00:20:47 +0530

    added mod ad array slices

---
+++ src/Arrays/main.go
@@ -0,0 +1,75 @@
+package main
+import (
+	_ "mymodule/geometry/rectangle"
+	_"fmt"
+	_"mymodule/arraysandslices"
+	_"strconv"
+    _"strings"
+	_"text/scanner"
+	_"mymodule/variadicfunctions"
+	_"mymodule/maps"
+	_"mymodule/pointers"
+	_"mymodule/DefiningStructs"
+	_"mymodule/gomethods"
+	_"mymodule/interfacesI"
+	_"mymodule/goRoutines"
+	_"mymodule/channels"
+	_"mymodule/BufferedChannels"
+
+	_"mymodule/DefiningStructs/structs"
+	"mymodule/udErrors"
+)
+func  main()  {
+	// a := rectangle.GetArea(1, 2);
+	// fmt.Println("a", a)
+	// s :=  arraysandslices.Dummy()
+	// fmt.Println(string(s), strconv.QuoteRune(s))
+
+	// arraysandslices.Dummy();
+
+	// arraysandslices.MoreAbooutSlices()
+	// variadicfunctions.MainFunc()
+	// maps.MainFunc()
+
+	// pointers.PointersFunc()
+	// pointers.CreateNewPointer()
+	// pointers.DereferencingPonter()
+	// pointers.ChangeValueUsingPointer()
+	// pointers.PassingPointer()
+	// pointers.CaputersAPointer()
+	// pointers.ArrayPointers()
+
+	// structs.MainStruct()
+	// structs.PointersAndStruct() 
+	// structs.MoreTypeOfStructs() 
+	// structs.NestedStructs() 
+	// structs.UsingExportedStruct() 
+	// structs.ComparingStruct() 
+	// structs.MapsStruct() 
+
+	// gomethods.EmployeeCaller()
+	// gomethods.ExMethods2()
+	// gomethods.GetValueOfAnonymousStruct()
+	// gomethods.GetValueOfAnonymousStruct()
+	// gomethods.GetsArea()
+	// gomethods.GetsArea1()
+	// gomethods.AddsTwoNum()
+
+	// interfacesI.CallerValue()
+	// interfacesI.CalulateCompanyExpenses()
+	// interfacesI.TestInterface()
+	// interfacesI.InterfaceSwitchCase()
+	// interfacesI.Caller()
+
+	// goRoutines.HelloRoutine()
+	// go goRoutines.Routine()
+	// goRoutines.StartRoutine()
+
+	// channels.TestChannel()
+	// channels.CreateChannel()
+	// channels.CalcCubesnSquares()
+	// BufferedChannels.Mainn()
+	// BufferedChannels.Mainn2()
+	udErrors.mainn()
+
+}
